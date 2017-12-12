e10f589f56a2edfa8127bafc6d9f62857a1ec27b
Nina Klemencic <nina4klemencic@gmail.com>
2017-12-11 16:29:36 -0800

    generalized random unique numbers

--- js/main.js
+++ js/main.js
@@ -1,28 +1,57 @@
 var $grid = $('#grid')
 var $squares = $('.square')
 var $topRow = $('#top-row')
+var $form = $('#form')
 var $playerName = $('#player-name')
 var $name = $('#name')
+var $start = $('#start')
 
 //generate 25 squares:
 for (var i = 0; i < 25; i += 1) {
-    $grid.append('<div class = square></square>')
+    $grid.append('<div class=square id=' +i+ '></square>')
 }
-
-//make buttons clickable:
-$grid.on('click', '.square', function(){
-    console.log("I'm clickable")
-})
-
-//add player name line after input:
-$playerName.on('submit', function(evt){
-   evt.preventDefault()
-   $name.text($playerName.val())
-})
 
 //create $topRow: clicks/score
 $topRow.append('<div class=row id=clicks>Clicks: </div>')
 $topRow.append('<div class=row id=score>Score: </div>')
+$topRow.append('<div class=row id=start>Start!</div>')
+
+//add player name line after he inputs his name:
+$form.on('submit', function(evt){
+    evt.preventDefault()
+    $name.text($playerName.val())
+    this.remove()
+ })
 
 //generate a random number 0-24:
+function randomInt(hi){
+    return Math.floor(Math.random() * hi)
+}
+
+//generate an array with 7 unique random numbers:
+var winningSquares = [] 
+while (winningSquares.length < 7 ) {
+    var randomNum = randomInt(25)
+    if (winningSquares.indexOf(randomNum) > -1) continue;
+    winningSquares[winningSquares.length] = randomNum;
+}
+
+//show 7 randomly highlighted squares on Start:
+$topRow.on('click', '#start', function(){
+    winningSquares.addClass('blue')
+    this.text("X")
+} )
+
+
+//create an EL for the squares
+$grid.on('click', '.square', function(){
+    console.log("I'm clickable")
+    
+    //check if the id is in the winning squares
+
+    //remember that the id of html element is string
+})
+
+
+
 
