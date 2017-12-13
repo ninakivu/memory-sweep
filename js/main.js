7b3ea8e0c86722448208c11bd27271f5ec274250
Nina Klemencic <nina4klemencic@gmail.com>
2017-12-12 16:58:33 -0800

    score after player1

--- js/main.js
+++ js/main.js
@@ -16,10 +16,11 @@ var $squares = $('.square')
 var $start = $('#start')
 
 //add player name line after he inputs his name:
+$playerName.focus()
 $form.on('submit', function(evt){
     evt.preventDefault()
     $name.text($playerName.val())
-    this.remove()
+    $form.hide()
  })
 
 //generate a random number 0-24:
@@ -37,6 +38,7 @@ while (winningSquares.length < 7 ) {
 
 var $score = $("#score")
 var $clicks = $("#clicks")
+var counter = 0
 
 $score.text(0)
 $clicks.text(7)
@@ -55,23 +57,35 @@ $start.on('click', function(){
 
         //+/- score and clicks accordingly
         $grid.on('click', '.square', function(){
-            // while ($clicks.text() > - 1) {
+            // while ($clicks.text() >=== - 1) {
             // }
-            $clicks.text(Number($clicks.text()) - 1)
-            if ($(this).hasClass('blue')){
-                console.log("You got it")
-                $(this).removeClass('white')
-                $score.text(Number($score.text()) + 1)
-            }
-            else {
-                console.log("Nope")
-                $score.text(Number($score.text()) - 1)
+            if(clicksRemaining()) {
+                $clicks.text(Number($clicks.text()) - 1)
+                if ($(this).hasClass('blue')){
+                    console.log("You got it")
+                    $(this).removeClass('white')
+                    $score.text(Number($score.text()) + 1)
+                }
+                else {
+                    console.log("Nope")
+                    $(this).css('backgroundColor', 'black')
+                    $score.text(Number($score.text()) - 1)
+                }
+            } else {
+                $('.square').off()
+                $('#instructions').text('Good job ' + $name.text() + '! Your score is ' + $score.text() + '. \n Next Player!')
+                $name.text($name.text() + ': ' + $score.text())
+                $form.show().text('')
+                $playerName.focus()
+                $('.square').css('backgroundColor', 'white')
             }
         })
-    }, 3000)
+    }, 2500)
 })
 
-
+function clicksRemaining() {
+    return Boolean(Number($clicks.text()))
+}
 
 
 
